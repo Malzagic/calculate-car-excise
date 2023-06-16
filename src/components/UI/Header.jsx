121ab2ec4643380ced21fa06e302195590769f9a
Malzagic <przemek2142@gmail.com>
2023-06-16 17:46:25 +0200

    add header

---
+++ src/components/UI/Header.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Header = () => {
+    return (
+        <header>
+            <div className="text-black bg-sky-500/100">
+                <h1 className="text-white text-2xl p-2">Kalkulator akcyzy samochodowej</h1>
+            </div>
+        </header>
+    )
+}
+
+
+export default Header;
